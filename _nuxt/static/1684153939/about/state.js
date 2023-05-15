6f3807bbaa1d21ec22c2ca5fedc19a43235f4a6f
zulhijaya <zulhijy@gmail.com>
2023-05-15 20:32:49 +0800

    fe99451

---
+++ _nuxt/static/1684153939/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fpersonal-portfolio\u002F_nuxt\u002Fstatic\u002F1684153939",layout:"none",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fpersonal-portfolio\u002F",assetsPath:"\u002Fpersonal-portfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
